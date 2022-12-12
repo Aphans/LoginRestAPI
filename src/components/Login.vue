1217488ff20173c0f9d377382b8b79ae1d276704
Aphans <rapperanqeel@hotmail.com>
2022-12-12 15:42:39 +0100

    request to json-server

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-for="el in usuario" v-bind:key="el.id">
+        {{el.usuario}}
+    </div>
+</template>
+<script setup>
+import {ref,computed,onMounted} from "vue";
+import axios from "axios";
+const usuario = ref([]);
+onMounted(()=>{
+    obtenerUsuario();
+})
+const obtenerUsuario = async()=>{
+    try{
+    const response = await axios.get('http://localhost:3000/usuarios?usuario=Pepe&contraseña=hello');
+    console.log(response);
+    usuario.value = response.data;
+    }catch(error){
+        console.log("No se ha obtenido el usuario");
+    }
+  }
+</script>
+<style scoped>
+</style>
